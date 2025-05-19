a4d450b710b0108ed8f108955824040fb6765e1d
bvilla844 <brayanvilla844@gmail.com>
2025-05-19 14:45:12 -0500

    db changes

--- src/__init__.py
+++ src/__init__.py
@@ -15,12 +15,7 @@ async def life_span(app: FastAPI):
     await init_db()
     yield
     print(f"server has been stopped..")
-    print("🧭 Diagnostic info (Render environment)")
-    print("📂 Current working directory:", os.getcwd())
-    print("📄 __file__:", __file__)
-    print("📂 BASE_DIR:", Path(__file__).resolve().parent)
-    print("📂 TEMPLATE_FOLDER:", Path(__file__).resolve().parent / "templates")
-    print("📁 TEMPLATE_FOLDER exists:", (Path(__file__).resolve().parent / "templates").exists())
+
 
 version = "v1"
 
@@ -28,6 +23,8 @@ app = FastAPI(
     title="Bookly",
     description="AREST API for a book review wev service",
     version=version,
+    lifespan=life_span
+
 )
 
 register_all_errors(app)
--- src/config.py
+++ src/config.py
@@ -1,7 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
-
 class Settings(BaseSettings):
     DATABASE_URL: str
     JWT_SECRET: str
